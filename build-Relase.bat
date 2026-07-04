ce063bede38ba24d356917cfbdc8c24c1e777665
Sam <shalahu@users.noreply.github.com>
2026-07-04 12:55:08 +0800

    Add files

---
+++ build-Relase.bat
@@ -0,0 +1,9 @@
+@echo off
+
+pushd SpotHusher
+
+dotnet publish -c Release -r win-x64 /p:PublishSingleFile=true --self-contained false /p:AssemblyName="SpotHusher_x64"
+
+dotnet publish -c Release -r win-x86 /p:PublishSingleFile=true --self-contained false /p:AssemblyName="SpotHusher_x86"
+
+popd
